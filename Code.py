189605ff28c2422a1319064efd51f38a9391bbe1
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-02-28 14:24:13 +1300

    Made it so code asks for customers name and addy and adds it to the database

--- Code.py
+++ Code.py
@@ -1,1 +1,17 @@
-print("Hello World")
+import sqlite3
+
+DATABASE = "pizza.db"
+
+Name = input("Welcome to Dominos! Please enter your name: ")
+Addy = input("Hey " + Name + ". Please enter your address or type Pick Up: ")
+
+# Connect to the database
+connect = sqlite3.connect(DATABASE)
+cursor = connect.cursor()
+
+# Insert new customer data
+sql = "INSERT INTO Customer (Name, Address) VALUES (?, ?)"
+cursor.execute(sql, (Name, Addy))
+connect.commit()
+
+pizza = input("Thanks for the info " + Name + ". From the list below please select what pizza you want")
---
+++ test.py
@@ -0,0 +1,3 @@
+Name = input("Welcome to Dominos! Please enter your name: ")
+
+Addy = input("Hey " + Name + ". Please enter your address or type Pick Up: ")
